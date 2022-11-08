64e6c32a2866a7f8efe8870a38fac1b81fe4eef0
KimGunWoo9595 <medneter@naver.com>
2022-11-08 13:10:50 +0900

    db insert 완료

--- src/main/java/com/mustache/bbs/domain/entity/Article.java
+++ src/main/java/com/mustache/bbs/domain/entity/Article.java
@@ -1,4 +1,29 @@
 package com.mustache.bbs.domain.entity;
 
-public class Article {
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+
+import javax.persistence.*;
+
+@Entity
+@NoArgsConstructor
+@Getter
+public class Article{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // db에 id 생성을 맡긴다
+    private Long id;
+
+    private String title;
+
+    private String content;
+
+    public Article(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
+
+
+
 }
