70dfcb4ba4d7fbc5788d4867964a014a25fac333
KimGunWoo9595 <medneter@naver.com>
2022-11-08 13:48:15 +0900

    id로 조회 추가

--- src/main/java/com/mustache/bbs/controller/ArticleController.java
+++ src/main/java/com/mustache/bbs/controller/ArticleController.java
@@ -6,10 +6,11 @@ import com.mustache.bbs.repository.ArticleRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/articles")
@@ -33,7 +34,30 @@ public class ArticleController {
         log.info(articleEntity.toString());
         // 2. Repository에게 Entity를 DB에 저장하게 한다.
         Article saved = articleRepository.save(articleEntity);
-        return "articles/new";
+        return "redirect:/articles/"+saved.getId();
     }
+
+    @GetMapping("/{id}")
+    public String read(@PathVariable Long id, Model model) {
+        log.info("id:{]",id);
+
+        Optional<Article> opArticle = articleRepository.findById(id);
+        Article articleEntity = opArticle.orElseGet(() -> {
+            return new Article("찾을 수 없는글의 제목","찾을 수 없는 글의 내용");
+        });
+        model.addAttribute("article", articleEntity);
+        return "articles/show";
+    }
+
+    @GetMapping
+    public  String getAll(Model model) {
+        List<Article> articleEntityList = articleRepository.findAll();
+        model.addAttribute("articleList", articleEntityList);
+        return "articles/index";
+    }
+
+
+
+
 
 }
