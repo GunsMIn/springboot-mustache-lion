a0f00f43abcb190d7fb9716cc94ec35fcaff85cd
KimGunWoo9595 <medneter@naver.com>
2022-11-08 13:09:09 +0900

    ArticleController

---
+++ src/main/java/com/mustache/bbs/controller/ArticleController.java
@@ -0,0 +1,39 @@
+package com.mustache.bbs.controller;
+
+import com.mustache.bbs.domain.dto.ArticleDto;
+import com.mustache.bbs.domain.entity.Article;
+import com.mustache.bbs.repository.ArticleRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+@RequestMapping("/articles")
+@Slf4j
+public class ArticleController {
+
+    private final ArticleRepository articleRepository;
+
+    public ArticleController(ArticleRepository articleRepository) {
+        this.articleRepository = articleRepository;
+    }
+
+    @GetMapping(value = "/new")
+    public String newArticleForm() {
+        return "articles/new";
+    }
+
+    @PostMapping(value="/posts")
+    public String createArticle(ArticleDto form) {
+        Article articleEntity = form.toEntity();
+        log.info(articleEntity.toString());
+        // 2. Repository에게 Entity를 DB에 저장하게 한다.
+        Article saved = articleRepository.save(articleEntity);
+        return "articles/new";
+    }
+
+}
