8e7a7fd213db0188eb726cc762de34c5ba86e89b
KimGunWoo9595 <medneter@naver.com>
2022-11-08 14:29:15 +0900

    [완성]컨트롤러 CRUD(삽입,조회,수정,삭제)

--- src/main/java/com/mustache/bbs/controller/ArticleController.java
+++ src/main/java/com/mustache/bbs/controller/ArticleController.java
@@ -6,6 +6,7 @@ import com.mustache.bbs.repository.ArticleRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
@@ -14,19 +15,19 @@ import java.util.Optional;
 
 @Controller
 @RequestMapping("/articles")
+@RequiredArgsConstructor
 @Slf4j
+@Transactional
 public class ArticleController {
 
     private final ArticleRepository articleRepository;
 
-    public ArticleController(ArticleRepository articleRepository) {
-        this.articleRepository = articleRepository;
-    }
-
+    //글 작성페이지
     @GetMapping(value = "/new")
     public String newArticleForm() {
         return "articles/new";
     }
+
 
     @PostMapping(value="/posts")
     public String createArticle(ArticleDto form) {
@@ -37,18 +38,22 @@ public class ArticleController {
         return "redirect:/articles/"+saved.getId();
     }
 
+    //id로 조회 페이지
     @GetMapping("/{id}")
     public String read(@PathVariable Long id, Model model) {
         log.info("id:{]",id);
 
         Optional<Article> opArticle = articleRepository.findById(id);
-        Article articleEntity = opArticle.orElseGet(() -> {
-            return new Article("찾을 수 없는글의 제목","찾을 수 없는 글의 내용");
-        });
-        model.addAttribute("article", articleEntity);
+        Article articleEntity = opArticle.orElse(null);
+        if (articleEntity != null) {
+            model.addAttribute("article", articleEntity);
+        }else{
+            return "error";
+        }
         return "articles/show";
     }
 
+    //전체글 리스트 페이지
     @GetMapping
     public  String getAll(Model model) {
         List<Article> articleEntityList = articleRepository.findAll();
@@ -56,8 +61,38 @@ public class ArticleController {
         return "articles/index";
     }
 
+    //글 수정 페이지
+    @GetMapping("/{id}/edit")
+    public String edit(@PathVariable Long id, Model model) {
+        Optional<Article> opArticle = articleRepository.findById(id);
+        Article articlesEntity = opArticle.orElseGet(() -> {
+            return new Article("찾을 수 없는글의 제목", "찾을 수 없는 글의 내용");
+        });
 
+        model.addAttribute("article", articlesEntity);
+        return "articles/edit";
+    }
 
+    //글 수정
+    @PostMapping("/update")
+    public String update(@ModelAttribute ArticleDto dto) {
+        log.info(dto.toString());
 
+        //피피티에서는 merge를 사용한거 같지만 변경감지 사용할 것 -> dirty cash
+        Long id = dto.getId();
+        Article article = articleRepository.findById(id).
+                orElseThrow(()->new IllegalArgumentException("해당 글이 업습니다"));
+        article.setTitle(dto.getTitle());
+        article.setContent(dto.getContent());
 
+        return "redirect:/articles";
+    }
+
+    @GetMapping("/{id}/delete")
+    public String delete(@PathVariable Long id) {
+        Optional<Article> opArticle = articleRepository.findById(id);
+        Article article = opArticle.orElse(null);
+        articleRepository.delete(article);
+        return "redirect:/articles";
+    }
 }
--- src/main/java/com/mustache/bbs/domain/entity/Article.java
+++ src/main/java/com/mustache/bbs/domain/entity/Article.java
@@ -2,21 +2,26 @@ package com.mustache.bbs.domain.entity;
 
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 import javax.persistence.*;
 
 @Entity
 @NoArgsConstructor
-@Getter
+@Getter @Setter
+@ToString
 public class Article{
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY) // db에 id 생성을 맡긴다
     private Long id;
 
+    @Column
     private String title;
 
+    @Column
     private String content;
 
     public Article(String title, String content) {
