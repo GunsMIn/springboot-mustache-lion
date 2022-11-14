bd3fbdbd6f2bd6678239862f904e8e0f6344c66b
KimGunWoo9595 <medneter@naver.com>
2022-11-14 17:53:00 +0900

    HospitalRestController

---
+++ src/main/java/com/mustache/bbs/controller/HospitalRestController.java
@@ -0,0 +1,32 @@
+package com.mustache.bbs.controller;
+
+import com.mustache.bbs.domain.dto.HospitalResponse;
+import com.mustache.bbs.domain.entity.Hospital;
+import com.mustache.bbs.repository.HospitalRepository;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/v1/hospitals")
+public class HospitalRestController {
+
+    private final HospitalRepository hospitalRepository;
+
+    public HospitalRestController(HospitalRepository hospitalRepository) {
+        this.hospitalRepository = hospitalRepository;
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<HospitalResponse> get(@PathVariable Integer id) {
+        Optional<Hospital> hospitalOptional = hospitalRepository.findById(id);
+        Hospital hospital = hospitalOptional.get();
+        return ResponseEntity.ok(Hospital.of(hospital));
+    }
+
+
+}
