b5b3aaa7a71f165305de65691cd33dc4232fabae
KimGunWoo9595 <medneter@naver.com>
2022-11-14 17:51:11 +0900

    product 엔티티

--- src/main/java/com/mustache/bbs/domain/entity/Product.java
+++ src/main/java/com/mustache/bbs/domain/entity/Product.java
@@ -1,4 +1,25 @@
 package com.mustache.bbs.domain.entity;
 
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+
+@Entity
+@Table(name="product") //생략 가능
 public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long number; // 상품번호
+
+    @Column(nullable = false)
+    private String name; // 상품이름
+
+    @Column(nullable = false)
+    private Integer price; // 상품가격
+
+    @Column(nullable = false)
+    private Integer stock; // 상품재고
+
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
 }
