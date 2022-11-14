8ce07f9ed5608e3c4af4b17855d235df1a71caaf
KimGunWoo9595 <medneter@naver.com>
2022-11-14 17:51:30 +0900

    Product repositry

--- src/main/java/com/mustache/bbs/repository/ProductRepository.java
+++ src/main/java/com/mustache/bbs/repository/ProductRepository.java
@@ -1,4 +1,10 @@
 package com.mustache.bbs.repository;
 
-public class ProductRepository {
+import com.mustache.bbs.domain.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface ProductRepository extends JpaRepository<Product,Long> {
+
 }
