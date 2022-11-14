a97c74954e1705f3fed93f55d25a0d1419fb9169
KimGunWoo9595 <medneter@naver.com>
2022-11-14 17:50:57 +0900

    sql

--- src/main/resources/sql/hospital.sql
+++ src/main/resources/sql/hospital.sql
@@ -1,2 +1,9 @@
 /*경기도 수원시경기도 수원시 피부과*/
-SELECT * FROM national_wide_hospital WHERE road_name_address LIKE "%경기도 수원시%" AND  hospital_name LIKE "%피부%"
+SELECT hospital_name, road_name_address FROM `likelion-db`.nation_wide_hospitals
+where road_name_address like "경기도 수원시%"
+and hospital_name like "%피부과%"
+
+/*특정 구의 - 보건진료소, 보건지소, 보건소 모두 찾기*/
+SELECT business_type_name, hospital_name, road_name_address
+FROM `likelion-db`.nation_wide_hospitals
+where business_type_name in ('보건소', '보건지소');
