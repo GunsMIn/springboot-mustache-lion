b6a746731a1f3c84b7228154ea915103a5609faa
KimGunWoo9595 <medneter@naver.com>
2022-11-14 17:50:24 +0900

    컬럼 매핑

--- src/main/java/com/mustache/bbs/domain/entity/Hospital.java
+++ src/main/java/com/mustache/bbs/domain/entity/Hospital.java
@@ -1,13 +1,17 @@
 package com.mustache.bbs.domain.entity;
 
+import com.mustache.bbs.domain.dto.HospitalResponse;
+import lombok.Getter;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
-@Entity
+@Entity @Getter
 @Table(name = "nation_wide_hospitals")
 public class Hospital {
+
     @Id
     private Integer id;
 
@@ -16,7 +20,19 @@ public class Hospital {
 
     @Column(name = "hospital_name")
     private String hospitalName;
+
+    @Column(name = "patient_room_count")
     private Integer patientRoomCount;
+    @Column(name = "total_number_of_beds")
     private Integer totalNumberOfBeds;
+    @Column(name = "business_type_name")
+    private String businessTypeName;
+    @Column(name = "total_area_size")
     private Float totalAreaSize;
+
+    public static HospitalResponse of(Hospital hospital) {
+        return new HospitalResponse(hospital.getId(),hospital.getHospitalName(),hospital.getRoadNameAddress());
+    }
+
+
 }
