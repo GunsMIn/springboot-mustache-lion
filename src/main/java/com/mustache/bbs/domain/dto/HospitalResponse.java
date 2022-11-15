7a0d7d37710d7cf4850d6d0c4553841f897cc2eb
KimGunWoo9595 <medneter@naver.com>
2022-11-15 13:05:13 +0900

    hospitalResponse

--- src/main/java/com/mustache/bbs/domain/dto/HospitalResponse.java
+++ src/main/java/com/mustache/bbs/domain/dto/HospitalResponse.java
@@ -8,24 +8,19 @@ import lombok.Getter;
 @Getter
 public class HospitalResponse {
     private Integer id;
-    private String roadNameAddress;
     private String hospitalName;
+    private String roadNameAddress;
     private Integer patientRoomCount;
     private Integer totalNumberOfBeds;
     private String businessTypeName;
     private Float totalAreaSize;
 
 
-    public HospitalResponse(Integer id, String roadNameAddress, String hospitalName) {
-        this.id = id;
-        this.roadNameAddress = roadNameAddress;
-        this.hospitalName = hospitalName;
-    }
 
-    public HospitalResponse(Integer id, String roadNameAddress, String hospitalName, Integer patientRoomCount, Integer totalNumberOfBeds, String businessTypeName, Float totalAreaSize) {
+    public HospitalResponse(Integer id, String hospitalName, String roadNameAddress, Integer patientRoomCount, Integer totalNumberOfBeds, String businessTypeName, Float totalAreaSize) {
         this.id = id;
-        this.roadNameAddress = roadNameAddress;
         this.hospitalName = hospitalName;
+        this.roadNameAddress = roadNameAddress;
         this.patientRoomCount = patientRoomCount;
         this.totalNumberOfBeds = totalNumberOfBeds;
         this.businessTypeName = businessTypeName;
