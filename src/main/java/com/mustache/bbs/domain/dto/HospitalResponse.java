efed442f1a4024711c41cc23d8145f24c4008784
KimGunWoo9595 <medneter@naver.com>
2022-11-15 14:28:01 +0900

    hospitalResponse 영업중,폐업,문의

--- src/main/java/com/mustache/bbs/domain/dto/HospitalResponse.java
+++ src/main/java/com/mustache/bbs/domain/dto/HospitalResponse.java
@@ -4,7 +4,7 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.Getter;
 
-@Builder
+
 @Getter
 public class HospitalResponse {
     private Integer id;
@@ -15,6 +15,13 @@ public class HospitalResponse {
     private String businessTypeName;
     private Float totalAreaSize;
 
+
+    //영업중,폐업중 -> hospital엔티티의 businessStatusCoded에 따라서 영업중,폐업중
+    private String businessStatusName;
+
+    public void setBusinessStatusName(String businessStatusName) {
+        this.businessStatusName = businessStatusName;
+    }
 
 
     public HospitalResponse(Integer id, String hospitalName, String roadNameAddress, Integer patientRoomCount, Integer totalNumberOfBeds, String businessTypeName, Float totalAreaSize) {
