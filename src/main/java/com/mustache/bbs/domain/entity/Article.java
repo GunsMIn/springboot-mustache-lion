3c280e222f0c83c03dd35286c44b933d06a6bd44
KimGunWoo9595 <medneter@naver.com>
2022-11-15 13:05:25 +0900

    article

--- src/main/java/com/mustache/bbs/domain/entity/Article.java
+++ src/main/java/com/mustache/bbs/domain/entity/Article.java
@@ -1,9 +1,6 @@
 package com.mustache.bbs.domain.entity;
 
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-import lombok.ToString;
+import lombok.*;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 import javax.persistence.*;
@@ -14,28 +11,17 @@ import static javax.persistence.CascadeType.*;
 
 @Entity
 @NoArgsConstructor
+@AllArgsConstructor
 @Getter @Setter
 @ToString
 public class Article{
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY) // db에 id 생성을 맡긴다
-    @Column(name = "article_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
-    @Column
     private String title;
-
-    @Column
     private String content;
 
-    @OneToMany(mappedBy = "article",cascade =REMOVE) // 글이 삭제되면 댓글도 삭제되어야하기때문에
-    List<Comment> comments = new ArrayList<>();
-
-    public Article(String title, String content) {
-        this.title = title;
-        this.content = content;
-    }
 
 
 
