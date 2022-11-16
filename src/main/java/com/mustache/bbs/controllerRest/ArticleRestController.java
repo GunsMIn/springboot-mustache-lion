5e9c419381d55b274bc809b39a25b3ae6eeb774c
KimGunWoo9595 <medneter@naver.com>
2022-11-16 16:44:22 +0900

    [CRUD] RestApi 완성 (select,insert,update,delete,selectAll)

--- src/main/java/com/mustache/bbs/controllerRest/ArticleRestController.java
+++ src/main/java/com/mustache/bbs/controllerRest/ArticleRestController.java
@@ -2,9 +2,10 @@ package com.mustache.bbs.controllerRest;
 
 import com.mustache.bbs.domain.dto.articleAdd.ArticleAddResponseDto;
 import com.mustache.bbs.domain.dto.articleAdd.ArticleAddRequestDto;
-import com.mustache.bbs.domain.dto.articleDelte.ArticleDeleteReqDto;
 import com.mustache.bbs.domain.dto.articleDelte.ArticleDeleteResponseDto;
+import com.mustache.bbs.domain.dto.articleListDto.ArticleListDto;
 import com.mustache.bbs.domain.dto.articleSelect.ArticleResponseDto;
+import com.mustache.bbs.domain.dto.articleListDto.Result;
 import com.mustache.bbs.domain.dto.articleUpdate.ArticleUpdateResponseDto;
 import com.mustache.bbs.domain.dto.articleUpdate.ArticleUpdateReqDto;
 import com.mustache.bbs.service.ArticleService;
@@ -13,6 +14,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @Slf4j
@@ -27,6 +30,7 @@ public class ArticleRestController {
         ArticleResponseDto articleDto = articleService.getArticle(id);
         return ResponseEntity.status(HttpStatus.OK).body(articleDto);
     }
+
     //add 컨트롤러
     //여기 컨트롤러에서는 dto가 2개가 사용이 될 것 이다. 
     // 1. 하나는 요청 dto  2. 다른 하나는 응답 dto
@@ -37,7 +41,7 @@ public class ArticleRestController {
     // ResponseEntity<ArticleAddResponseDto> 로 반환한다
     @PostMapping("/api/articles")
     public ResponseEntity<ArticleAddResponseDto> add(@RequestBody ArticleAddRequestDto articleRequestDto) {
-        log.info("article : {}",articleRequestDto);
+        log.info("article : {}", articleRequestDto);
         ArticleAddResponseDto articleDto = articleService.addArticle(articleRequestDto);
         return ResponseEntity.ok().body(articleDto);
     }
@@ -52,8 +56,15 @@ public class ArticleRestController {
     }
 
     @DeleteMapping("/api/articles/{id}")
-    public ResponseEntity<ArticleDeleteResponseDto> delete(@PathVariable Long id){
+    public ResponseEntity<ArticleDeleteResponseDto> delete(@PathVariable Long id) {
         ArticleDeleteResponseDto articleDeleteResponseDto = articleService.deleteArticle(id);
         return ResponseEntity.status(HttpStatus.OK).body(articleDeleteResponseDto);
+    }
+
+    @GetMapping("/api/articles")
+    public Result selectAll() {
+        List<ArticleListDto> articleListDtoList = articleService.selectAll();
+        //개수 까지 알 수 있다.
+        return new Result(articleListDtoList,articleListDtoList.size());
     }
 }
