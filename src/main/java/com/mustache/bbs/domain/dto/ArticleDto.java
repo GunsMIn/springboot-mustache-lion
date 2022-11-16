397db8a262ad77466c5979164b02fc1b7674e2d2
KimGunWoo9595 <medneter@naver.com>
2022-11-16 11:22:01 +0900

    bulider생성

--- src/main/java/com/mustache/bbs/domain/dto/ArticleDto.java
+++ src/main/java/com/mustache/bbs/domain/dto/ArticleDto.java
@@ -2,12 +2,14 @@ package com.mustache.bbs.domain.dto;
 
 import com.mustache.bbs.domain.entity.Article;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.ToString;
 
 
 @Getter
 @ToString
+@Builder
 @AllArgsConstructor
 public class ArticleDto {
 
