21679e266a239d5d0658406f038dc9f7995c8599
KimGunWoo9595 <medneter@naver.com>
2022-11-16 14:49:12 +0900

    add 컨트롤러 dto  사용이유 주석

--- src/main/java/com/mustache/bbs/controllerRest/ArticleRestController.java
+++ src/main/java/com/mustache/bbs/controllerRest/ArticleRestController.java
@@ -1,7 +1,7 @@
 package com.mustache.bbs.controllerRest;
 
-import com.mustache.bbs.domain.dto.articleAdd.ArticleDto;
-import com.mustache.bbs.domain.dto.articleAdd.ArticleRequestDto;
+import com.mustache.bbs.domain.dto.articleAdd.ArticleAddResponseDto;
+import com.mustache.bbs.domain.dto.articleAdd.ArticleAddRequestDto;
 import com.mustache.bbs.service.ArticleService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -16,16 +16,25 @@ public class ArticleRestController {
 
     private final ArticleService articleService;
 
+    //단건 조회 컨트롤러
     @GetMapping("/api/articles/{id}")
-    public ResponseEntity<ArticleDto> get(@PathVariable Long id) {
-        ArticleDto articleDto = articleService.getArticle(id);
+    public ResponseEntity<ArticleAddResponseDto> get(@PathVariable Long id) {
+        ArticleAddResponseDto articleDto = articleService.getArticle(id);
         return ResponseEntity.status(HttpStatus.OK).body(articleDto);
     }
 
+    //add 컨트롤러
+    //여기 컨트롤러에서는 dto가 2개가 사용이 될 것 이다. 
+    // 1. 하나는 요청 dto  2. 다른 하나는 응답 dto
+    // 또 중요한 키 포인트는 JpaRepositroy<Article,Long> 을 사용해야하기 때문에
+    // dto에서 엔티티로 변환 해주는 메소드 또한 필요한다
+    // 그래서 컨트롤러 매개변수에서는 ArticleAddRequestDto 받고 Service 단에서 Article엔티티로 바꿔준다
+    // 그리고 Article 엔티티에서 ArticleAddResponseDto 바꿔주는 메소드를 이용하여 
+    // ResponseEntity<ArticleAddResponseDto> 로 반환한다
     @PostMapping("/api/articles")
-    public ResponseEntity<ArticleDto>add(@RequestBody ArticleRequestDto articleRequestDto) {
+    public ResponseEntity<ArticleAddResponseDto> add(@RequestBody ArticleAddRequestDto articleRequestDto) {
         log.info("article : {}",articleRequestDto);
-        ArticleDto articleDto = articleService.addArticle(articleRequestDto);
+        ArticleAddResponseDto articleDto = articleService.addArticle(articleRequestDto);
         return ResponseEntity.ok().body(articleDto);
     }
 }
