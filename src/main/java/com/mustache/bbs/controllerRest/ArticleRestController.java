5a726b3f2b0d376293f7943dc81c8452f212d789
KimGunWoo9595 <medneter@naver.com>
2022-11-16 14:06:29 +0900

    add Restcontroller

--- src/main/java/com/mustache/bbs/controllerRest/ArticleRestController.java
+++ src/main/java/com/mustache/bbs/controllerRest/ArticleRestController.java
@@ -1,25 +1,31 @@
 package com.mustache.bbs.controllerRest;
 
-import com.mustache.bbs.domain.dto.ArticleDto;
-import com.mustache.bbs.domain.entity.Article;
+import com.mustache.bbs.domain.dto.articleAdd.ArticleDto;
+import com.mustache.bbs.domain.dto.articleAdd.ArticleRequestDto;
 import com.mustache.bbs.service.ArticleService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@Slf4j
 @RequiredArgsConstructor
 public class ArticleRestController {
 
     private final ArticleService articleService;
 
-    @GetMapping("/api/article/{id}")
+    @GetMapping("/api/articles/{id}")
     public ResponseEntity<ArticleDto> get(@PathVariable Long id) {
         ArticleDto articleDto = articleService.getArticle(id);
         return ResponseEntity.status(HttpStatus.OK).body(articleDto);
     }
 
+    @PostMapping("/api/articles")
+    public ResponseEntity<ArticleDto>add(@RequestBody ArticleRequestDto articleRequestDto) {
+        log.info("article : {}",articleRequestDto);
+        ArticleDto articleDto = articleService.addArticle(articleRequestDto);
+        return ResponseEntity.ok().body(articleDto);
+    }
 }
