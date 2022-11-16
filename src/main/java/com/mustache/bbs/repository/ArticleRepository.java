de36ded5ba96327653b02d6f5d97489d5c4f233d
KimGunWoo9595 <medneter@naver.com>
2022-11-16 15:54:57 +0900

    articleRepositroy 리턴 타입  쿼리메소드 추가

--- src/main/java/com/mustache/bbs/repository/ArticleRepository.java
+++ src/main/java/com/mustache/bbs/repository/ArticleRepository.java
@@ -2,10 +2,10 @@ package com.mustache.bbs.repository;
 
 import com.mustache.bbs.domain.entity.Article;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Repository;
 
 
 public interface ArticleRepository extends JpaRepository<Article,Long> {
+
+    Long delete(Long id);
 
 }
