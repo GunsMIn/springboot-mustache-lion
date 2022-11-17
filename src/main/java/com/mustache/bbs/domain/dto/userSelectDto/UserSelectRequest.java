230a8734c61aca64b45e80b4a9e9783eb770081e
KimGunWoo9595 <medneter@naver.com>
2022-11-17 14:14:43 +0900

    userResponseDto 빌더추가

--- src/main/java/com/mustache/bbs/domain/dto/userSelectDto/UserSelectRequest.java
+++ src/main/java/com/mustache/bbs/domain/dto/userSelectDto/UserSelectRequest.java
@@ -2,11 +2,13 @@ package com.mustache.bbs.domain.dto.userSelectDto;
 
 import com.mustache.bbs.domain.entity.User;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import net.bytebuddy.asm.Advice;
 
 @Data
+@Builder
 @AllArgsConstructor
 @NoArgsConstructor
 public class UserSelectRequest {
