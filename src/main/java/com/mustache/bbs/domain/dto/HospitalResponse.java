f46378514f8afba287f82d442d5e16ebafc2923f
KimGunWoo9595 <medneter@naver.com>
2022-11-17 17:30:20 +0900

    빌더주석

--- src/main/java/com/mustache/bbs/domain/dto/HospitalResponse.java
+++ src/main/java/com/mustache/bbs/domain/dto/HospitalResponse.java
@@ -1,10 +1,15 @@
 package com.mustache.bbs.domain.dto;
 
-import lombok.Builder;
-import lombok.Data;
-import lombok.Getter;
-
-
+import lombok.*;
+//빌더는 모든 멤버변수를 가지는 생성자가 필요하기 때문에 lombok을 사용할 경우 @AllArgsConstructor를 사용 필수!
+/*HospitalResponse 오브젝트는 .builder() 패턴을 이용해 생성할 수 있습니다.
+몇번째 무엇이 들어가야 하는지를 일일히 안맞춰주어도 됩니다.
+그리고 필요한 파라메터만 넣고도 빌드 할 수 있습니다.
+Lombok의 기능 입니다. -=> test에서 사용
+*/
+@Builder/**/
+@NoArgsConstructor
+@AllArgsConstructor
 @Getter
 public class HospitalResponse {
     private Integer id;
@@ -15,14 +20,12 @@ public class HospitalResponse {
     private String businessTypeName;
     private Float totalAreaSize;
 
-
-    //영업중,폐업중 -> hospital엔티티의 businessStatusCoded에 따라서 영업중,폐업중
+    //영업중,폐업중 -> hospital엔티티의 businessStatusCode 에 따라서 영업중,폐업중
     private String businessStatusName;
 
     public void setBusinessStatusName(String businessStatusName) {
         this.businessStatusName = businessStatusName;
     }
-
 
     public HospitalResponse(Integer id, String hospitalName, String roadNameAddress, Integer patientRoomCount, Integer totalNumberOfBeds, String businessTypeName, Float totalAreaSize) {
         this.id = id;
