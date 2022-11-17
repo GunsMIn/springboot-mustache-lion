6c302ebd013e4d8f5e0ce6e4560d472f477d9d82
KimGunWoo9595 <medneter@naver.com>
2022-11-17 13:21:32 +0900

    userRepository추가(이름중복체크 필요 메소드)

--- src/main/java/com/mustache/bbs/repository/UserRepository.java
+++ src/main/java/com/mustache/bbs/repository/UserRepository.java
@@ -3,5 +3,9 @@ package com.mustache.bbs.repository;
 import com.mustache.bbs.domain.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface UserRepository extends JpaRepository<User,Long> {
+
+    List<User>findByUsername(String username);
 }
