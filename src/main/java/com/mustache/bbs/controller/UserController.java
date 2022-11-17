88c6d5e5a5b38771cd4057693e8ab158cb9d4d12
KimGunWoo9595 <medneter@naver.com>
2022-11-17 13:08:26 +0900

    post user 등록하기

--- src/main/java/com/mustache/bbs/controller/UserController.java
+++ src/main/java/com/mustache/bbs/controller/UserController.java
@@ -1,13 +1,13 @@
 package com.mustache.bbs.controller;
 
+import com.mustache.bbs.domain.dto.userAdd.UserAddRequest;
+import com.mustache.bbs.domain.dto.userAdd.UserAddResponse;
 import com.mustache.bbs.domain.dto.userSelectDto.UserSelectRequest;
 import com.mustache.bbs.domain.dto.userSelectDto.UserSelectResponse;
 import com.mustache.bbs.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -15,10 +15,16 @@ public class UserController {
 
     private final UserService userService;
 
-    @GetMapping("/api/users")
-    public ResponseEntity<UserSelectResponse> getUser(@RequestBody UserSelectRequest userSelectRequest) {
-        UserSelectResponse userSelectResponse = userService.findUser(userSelectRequest);
+    @GetMapping("/api/users/{id}")
+    public ResponseEntity<UserSelectResponse> getUser(@PathVariable Long id) {
+        UserSelectResponse userSelectResponse = userService.findUser(id);
 
         return ResponseEntity.ok().body(userSelectResponse);
+    }
+
+    @PostMapping("/api/users")
+    public ResponseEntity<UserAddResponse> addUser(@RequestBody UserAddRequest userAddRequest) {
+        UserAddResponse userAddResponse = userService.addUser(userAddRequest);
+        return ResponseEntity.ok().body(userAddResponse);
     }
 }
