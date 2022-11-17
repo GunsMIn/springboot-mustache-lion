7bb70668f118b11514fb7048871430264ac291b0
KimGunWoo9595 <medneter@naver.com>
2022-11-17 14:16:00 +0900

    POST /api/users 컨트롤러 TDD(성공)

--- src/test/java/com/mustache/bbs/controller/UserControllerTest.java
+++ src/test/java/com/mustache/bbs/controller/UserControllerTest.java
@@ -2,22 +2,26 @@ package com.mustache.bbs.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.mustache.bbs.domain.dto.userAdd.UserAddRequest;
+import com.mustache.bbs.domain.dto.userAdd.UserAddResponse;
 import com.mustache.bbs.domain.dto.userSelectDto.UserSelectRequest;
 import com.mustache.bbs.domain.dto.userSelectDto.UserSelectResponse;
 import com.mustache.bbs.service.UserService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentMatchers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-
-import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import org.springframework.http.MediaType;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.verify;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(UserController.class)
@@ -59,6 +63,27 @@ class UserControllerTest {
         verify(userService).findUser(userId);
     }
 
+    @Test
+    @DisplayName("회원 넣기 컨트롤러 테스트")
+    void add() throws Exception {
+        UserAddRequest userAddRequest =
+                 new UserAddRequest("김건우", "kk6251");
+        given(userService.addUser(any(UserAddRequest.class)))
+                .willReturn(new UserAddResponse(10L,userAddRequest.getUsername(), userAddRequest.getPassword()));
 
+        mockMvc.perform(post("/api/users")
+                .content(objectMapper.writeValueAsBytes(userAddRequest))
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").exists())
+                .andExpect(jsonPath("$.username").exists())
+                .andExpect(jsonPath("$.username").value("김건우"))
+                .andExpect(jsonPath("$.password").exists())
+                .andExpect(jsonPath("$.password").value("kk6251"))
+                .andDo(print());
+
+        verify(userService).addUser(ArgumentMatchers.refEq(userAddRequest));
+
+    }
 
 }
