cb45e3b54dd23df098dbacff064d201e05e96c53
KimGunWoo9595 <medneter@naver.com>
2022-11-18 13:34:26 +0900

    Page<Hospital> 검색,페이징 처리

--- src/main/java/com/mustache/bbs/repository/HospitalRepository.java
+++ src/main/java/com/mustache/bbs/repository/HospitalRepository.java
@@ -2,9 +2,9 @@ package com.mustache.bbs.repository;
 
 import com.mustache.bbs.domain.entity.Hospital;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.awt.print.Pageable;
 import java.util.List;
 
 public interface HospitalRepository extends JpaRepository<Hospital, Integer> {
@@ -12,8 +12,8 @@ public interface HospitalRepository extends JpaRepository<Hospital, Integer> {
     //*특정 구의 - 보건진료소, 보건지소, 보건소 모두 찾기*//*
     List<Hospital> findByBusinessTypeNameIn(List<String> businessTypes);
 
-    //포함되는 도로명으로 list 병원 찾기
-    List<Hospital> findByRoadNameAddressContaining(String keyword); // 포함
+    //포함되는 도로명으로 list 병원 찾기 ->
+    Page<Hospital> findByRoadNameAddressContaining(String keyword,Pageable pageable); // 포함
 
     //포함되는 병원명으로 병원리스트 찾기
     List<Hospital> findByHospitalNameContaining(String keyword); // 포함값
@@ -29,6 +29,7 @@ public interface HospitalRepository extends JpaRepository<Hospital, Integer> {
 
     //병상 수가 10개 이상 20개 미만인 병원을 모두 찾기 오름차순
     List<Hospital> findByTotalNumberOfBedsBetweenOrderByTotalNumberOfBeds(Integer start,Integer end);
+
 
 
 }
