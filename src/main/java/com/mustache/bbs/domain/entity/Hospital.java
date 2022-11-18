e906f746a4043beca52f54442f9782ad2dfcbc24
KimGunWoo9595 <medneter@naver.com>
2022-11-18 11:18:19 +0900

    생성자 추가

--- src/main/java/com/mustache/bbs/domain/entity/Hospital.java
+++ src/main/java/com/mustache/bbs/domain/entity/Hospital.java
@@ -1,7 +1,9 @@
 package com.mustache.bbs.domain.entity;
 
 import com.mustache.bbs.domain.dto.HospitalResponse;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -10,23 +12,23 @@ import javax.persistence.Table;
 
 @Entity @Getter
 @Table(name = "nation_wide_hospitals")
+@AllArgsConstructor @NoArgsConstructor
 public class Hospital {
 
     @Id
     private Integer id;
-
     @Column(name = "road_name_address")
     private String roadNameAddress;
-
     @Column(name = "hospital_name")
     private String hospitalName;
-
     @Column(name = "patient_room_count")
     private Integer patientRoomCount;
     @Column(name = "total_number_of_beds")
     private Integer totalNumberOfBeds;
     @Column(name = "business_type_name")
     private String businessTypeName;
+    @Column(name = "business_status_code")
+    private Integer businessStatusCode; // 비지니스 로직에서 businessStatusCode로 영업,폐업 결정
     @Column(name = "total_area_size")
     private Float totalAreaSize;
 
