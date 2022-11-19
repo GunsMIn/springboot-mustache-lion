b37f5a970f349d1a12203b32873afbd5c438cca9
KimGunWoo9595 <medneter@naver.com>
2022-11-19 09:57:40 +0900

    articleRepository

--- src/main/java/com/mustache/bbs/repository/ArticleRepository.java
+++ src/main/java/com/mustache/bbs/repository/ArticleRepository.java
@@ -3,9 +3,8 @@ package com.mustache.bbs.repository;
 import com.mustache.bbs.domain.entity.Article;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 
 public interface ArticleRepository extends JpaRepository<Article,Long> {
-
-   /* Long delete(Long id);*/
-
 }
