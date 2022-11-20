1aebdb56f772baec158442a5e11677ce214e0ea2
KimGunWoo9595 <medneter@naver.com>
2022-11-20 12:06:26 +0900

    빌더패턴추가

--- src/main/java/com/mustache/bbs/domain/dto/articleAdd/ArticleAddRequestDto.java
+++ src/main/java/com/mustache/bbs/domain/dto/articleAdd/ArticleAddRequestDto.java
@@ -1,18 +1,31 @@
 package com.mustache.bbs.domain.dto.articleAdd;
 
 import com.mustache.bbs.domain.entity.Article;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 @Data
-@AllArgsConstructor
-@NoArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class ArticleAddRequestDto {
     private String title;
     private String content;
 
     //requesrDto를 엔티티로!
+    //여기가 핵심!
+
+    @Builder
+    public ArticleAddRequestDto(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
+
+    public Article toEntity() {
+        Article article = Article.builder()
+                .title(title)
+                .content(content)
+                .build();
+        return article;
+    }
+
     public Article toEntity(ArticleAddRequestDto dto) {
         return new Article(dto.title,dto.content);
     }
