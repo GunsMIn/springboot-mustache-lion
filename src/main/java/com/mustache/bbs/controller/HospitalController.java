22d8e3dd8ea8a2d311c864aa9426a610a283223e
KimGunWoo9595 <medneter@naver.com>
2022-11-20 20:50:52 +0900

    병원명 검색+지역명 검색+페이징처리

--- src/main/java/com/mustache/bbs/controller/HospitalController.java
+++ src/main/java/com/mustache/bbs/controller/HospitalController.java
@@ -1,6 +1,7 @@
 package com.mustache.bbs.controller;
 
 
+import com.mustache.bbs.domain.dto.HospitalResponse;
 import com.mustache.bbs.domain.entity.Hospital;
 import com.mustache.bbs.domain.entity.Review;
 import com.mustache.bbs.repository.HospitalRepository;
@@ -24,37 +25,33 @@ import java.util.Optional;
 
 @Controller
 @RequestMapping("/hospitals")
+@RequiredArgsConstructor
 @Slf4j
 public class HospitalController {
     private final HospitalRepository hospitalRepository;
     private final ReviewRepository reviewRepository;
 
-    public HospitalController(HospitalRepository hospitalRepository, ReviewRepository reviewRepository) {
-        this.hospitalRepository = hospitalRepository;
-        this.reviewRepository = reviewRepository;
-    }
-
-    /**
-     * @PageableDefault
-     * <p>
-     * ○ size : 한 페이지에 담을 모델의 수를 정할 수 있다. 기본 값은 10이다.
-     * ○ sort : 정렬의 기준이 되는 속성을 정한다.
-     * ○ direction : 오름차순과 내림차순 중 기준을 선택할 수 있다.
-     * ○ Pageable pageable : PageableDefault 값을 갖고 있는 변수를 선언한다.
-     */
-
     //키워드(지역명) 검색 기능 + 페이징
     @GetMapping
-    public String list(@RequestParam(required = false) String keyword,Pageable pageable,Model model)  {
+    public String list(@RequestParam(required = false) String keyword,
+                       @RequestParam(required = false) String hospitalName,
+                       Pageable pageable,Model model)  {
         Page<Hospital> hospitals;
-        if(keyword!=null) hospitals = hospitalRepository.findByRoadNameAddressContaining(keyword,pageable);
-        else hospitals = hospitalRepository.findAll(pageable);
+
+        if (hospitalName !=null) {//병원 이름으로 검색시
+            hospitals = hospitalRepository.findByHospitalNameContaining(hospitalName, pageable);
+        } else if (keyword != null) {//도로명으로 검색시
+            hospitals = hospitalRepository.findByRoadNameAddressContaining(keyword, pageable);
+        } else {// 검색X
+            hospitals = hospitalRepository.findAll(pageable);
+        }
 
         log.info("지역명으로 검색한 검색어 : {}",keyword);
         log.info("size:{}", hospitals.getSize());
 
         model.addAttribute("hospitals", hospitals);
         model.addAttribute("keyword", keyword);
+        model.addAttribute("hospitalName", hospitalName);
         model.addAttribute("previous", pageable.previousOrFirst().getPageNumber());
         model.addAttribute("next", pageable.next().getPageNumber());
         return "hospital/list";
@@ -62,10 +59,11 @@ public class HospitalController {
 
     @GetMapping("/{id}")
     public String show(@PathVariable Integer id, Model model) {
-        Optional<Hospital> hospital = hospitalRepository.findById(id);
+        Optional<Hospital> hospitalOptional = hospitalRepository.findById(id);
+        Hospital hospital = hospitalOptional.get();
         List<Review> reviews = reviewRepository.findByHospitalId(id);
         log.info("review cnt:{}", reviews.size());
-        model.addAttribute("hospital", hospital.get());
+        model.addAttribute("hospital", hospital);
         model.addAttribute("reviews", reviews);
         return "hospital/show";
     }
