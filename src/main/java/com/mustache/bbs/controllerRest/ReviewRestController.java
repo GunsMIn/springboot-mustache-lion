60d491c2475db4165f42ba3962d0e632b0f74e3b
KimGunWoo9595 <medneter@naver.com>
2022-11-25 11:15:18 +0900

    리부 단건 조회 / 리뷰 리스트 조회

--- src/main/java/com/mustache/bbs/controllerRest/ReviewRestController.java
+++ src/main/java/com/mustache/bbs/controllerRest/ReviewRestController.java
@@ -1,39 +1,47 @@
 package com.mustache.bbs.controllerRest;
 
 import com.mustache.bbs.domain.dto.reviewDto.ReviewCreateRequest;
-import com.mustache.bbs.domain.dto.reviewDto.ReviewDto;
+import com.mustache.bbs.domain.dto.reviewDto.ReviewCreateResponse;
+import com.mustache.bbs.domain.dto.reviewDto.ReviewSelectResponse;
+import com.mustache.bbs.domain.entity.Hospital;
 import com.mustache.bbs.domain.entity.Review;
+import com.mustache.bbs.repository.HospitalRepository;
 import com.mustache.bbs.repository.ReviewRepository;
 import com.mustache.bbs.service.ReviewService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Optional;
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
 @Slf4j
-@RequestMapping("/api/reviews")
 public class ReviewRestController {
 
     private final ReviewService reviewService;
-    private final ReviewRepository reviewRepository;
 
-    @GetMapping("/{id}")
-    public ReviewDto getReview(@PathVariable Long id) {
+    //리뷰 id로 리뷰와 병원정보조회
+    @GetMapping("/api/{id}/reviews")
+    public ReviewSelectResponse getReview(@PathVariable Long id) {
         Review review = reviewService.findById(id);
-        ReviewDto reviewDto = new ReviewDto(review);
+        log.info("리뷰: {}",review);
+        ReviewSelectResponse reviewDto = new ReviewSelectResponse(review);
         return reviewDto;
     }
 
-
-    @PostMapping
-    public Review addReview(@RequestBody Review review) {
-        log.info("review :{}",review);
-        Long reviewId = reviewService.save(review);
-        Review savedReview = reviewService.findById(reviewId);
-        return savedReview;
+    @PostMapping("/api/{id}/reviews")
+    public ResponseEntity<ReviewCreateResponse> writeReview(@PathVariable Integer id, @RequestBody ReviewCreateRequest reviewCreateRequest) {
+        ReviewCreateResponse reviewCreateResponse = reviewService.add(reviewCreateRequest);
+        return ResponseEntity.ok().body(reviewCreateResponse);
     }
+
+    @GetMapping("/api/reviews")
+    public  ResponseEntity<List<ReviewSelectResponse>> getReviewList() {
+        List<ReviewSelectResponse> responseList = reviewService.findAll();
+        return ResponseEntity.ok().body(responseList);
+    }
+
 }
 
