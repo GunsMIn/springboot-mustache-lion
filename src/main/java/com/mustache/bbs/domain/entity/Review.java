8d7c74ab52eee71ce541975af0c4d9a2f6600177
KimGunWoo9595 <medneter@naver.com>
2022-11-25 11:22:50 +0900

    user Bulider pattern

--- src/main/java/com/mustache/bbs/domain/entity/Review.java
+++ src/main/java/com/mustache/bbs/domain/entity/Review.java
@@ -1,17 +1,13 @@
 package com.mustache.bbs.domain.entity;
 
 import com.mustache.bbs.domain.dto.reviewDto.ReviewCreateRequest;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import javax.persistence.*;
 
 import static javax.persistence.FetchType.*;
 
-@Entity
-@AllArgsConstructor
+@Entity @ToString
 @NoArgsConstructor
 @Getter
 @Setter
@@ -27,13 +23,20 @@ public class Review {
     @Column(name="user_name")
     private String userName;
 
-    @ManyToOne(fetch = LAZY,cascade = CascadeType.ALL) // 리뷰가 병원의 fk를 갖고있다.
+    @ManyToOne(fetch = LAZY) // 리뷰가 병원의 fk를 갖고있다.
     @JoinColumn(name = "hospital_id") // 연관관계의 주인
     private Hospital hospital;
 
-    public void setHospital(Hospital hospital) {
+    public void addReview(Hospital hospital) {
         this.hospital = hospital;
         hospital.getReviews().add(this);
     }
-
+    @Builder
+    public Review(Long id, String title, String content, String userName, Hospital hospital) {
+        this.id = id;
+        this.title = title;
+        this.content = content;
+        this.userName = userName;
+        this.hospital = hospital;
+    }
 }
