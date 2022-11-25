cf0f7f8bc95f54b2bc79c65448140042645b0af2
KimGunWoo9595 <medneter@naver.com>
2022-11-25 11:14:39 +0900

    reviewService

---
+++ src/main/java/com/mustache/bbs/domain/dto/reviewDto/ReviewSelectResponse.java
@@ -0,0 +1,4 @@
+package com.mustache.bbs.domain.dto.reviewDto;
+
+public class ReviewSelectResponse {
+}
--- src/main/java/com/mustache/bbs/service/ReviewService.java
+++ src/main/java/com/mustache/bbs/service/ReviewService.java
@@ -1,30 +1,61 @@
 package com.mustache.bbs.service;
 
+import com.mustache.bbs.domain.dto.reviewDto.ReviewCreateRequest;
+import com.mustache.bbs.domain.dto.reviewDto.ReviewCreateResponse;
+import com.mustache.bbs.domain.dto.reviewDto.ReviewSelectResponse;
+import com.mustache.bbs.domain.entity.Hospital;
 import com.mustache.bbs.domain.entity.Review;
+import com.mustache.bbs.repository.HospitalRepository;
 import com.mustache.bbs.repository.ReviewRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
+@Transactional
 public class ReviewService {
 
     private final ReviewRepository reviewRepository;
+    private final HospitalRepository hospitalRepository;
 
-    @Transactional
-    public Long save(Review review) {
+
+    public ReviewCreateResponse add(ReviewCreateRequest reviewCreateRequest) {
+        //hospital 먼저 조회해주자
+        int hospitalId = reviewCreateRequest.getHospitalId();
+        Optional<Hospital> hospitalOptional = hospitalRepository.findById(hospitalId);
+        Hospital hospital = hospitalOptional.get(); // 해당 병원 객체
+
+        //dto -> entity
+        Review review = Review.builder()
+                .title(reviewCreateRequest.title)
+                .content(reviewCreateRequest.content)
+                .userName(reviewCreateRequest.userName)
+                .hospital(hospital).build();
+        //save(Review)
         Review savedReview = reviewRepository.save(review);
 
-        return save.getId();
+        ReviewCreateResponse reviewCreateResponse = new ReviewCreateResponse(savedReview);
+        return reviewCreateResponse;
     }
 
     public Review findById(Long id) {
-        Optional<Review> byId = reviewRepository.findById(id);
-        Review review = byId.get();
+        Optional<Review> reviewOptional = reviewRepository.findById(id);
+        Review review = reviewOptional.get();
         return review;
+    }
+
+    public List<ReviewSelectResponse> findAll() {
+        List<Review> reviews = reviewRepository.findAll();
+
+        List<ReviewSelectResponse> reviewSelectResponses =
+                reviews.stream().map(r -> new ReviewSelectResponse(r)).collect(Collectors.toList());
+
+        return reviewSelectResponses;
     }
 
 }
