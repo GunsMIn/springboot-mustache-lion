145280596ff1a4608ea519b9e9d1a00ed0ca2bf4
KimGunWoo9595 <medneter@naver.com>
2022-11-27 21:16:46 +0900

    BookController(add)

--- src/main/java/com/mustache/bbs/controllerRest/BookRestController.java
+++ src/main/java/com/mustache/bbs/controllerRest/BookRestController.java
@@ -1,17 +1,12 @@
 package com.mustache.bbs.controllerRest;
 
-import com.mustache.bbs.domain.dto.bookDto.BookListDto;
-import com.mustache.bbs.domain.dto.bookDto.BookSelectDto;
-import com.mustache.bbs.domain.dto.bookDto.Result;
+import com.mustache.bbs.domain.dto.bookDto.*;
 import com.mustache.bbs.repository.BookRepository;
 import com.mustache.bbs.service.BookService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -37,6 +32,19 @@ public class BookRestController {
         BookSelectDto bookSelectDto = bookService.findById(id);
         return ResponseEntity.ok().body(bookSelectDto);
     }
+
+    //책 등록
+    @PostMapping
+    public ResponseEntity<BookCreateResponse> add(@RequestBody BookCreateRequest bookCreateRequest) {
+        log.info("controller - > dto :{}",bookCreateRequest);
+        BookCreateResponse bookCreateResponse = bookService.addBook(bookCreateRequest);
+        return ResponseEntity.ok().body(bookCreateResponse);
+    }
+    /* {
+          "publisherId":1,
+          "authorId":100,
+          "name":"면접에 합격하는 cs 지식"
+        }   */
 
 
 }
