f2d6b6085b43d17dffb9be0c46b8fa731c1eeea3
KimGunWoo9595 <medneter@naver.com>
2022-11-28 13:46:29 +0900

    Response 추상화

--- src/main/java/com/mustache/bbs/domain/Response.java
+++ src/main/java/com/mustache/bbs/domain/Response.java
@@ -1,4 +1,23 @@
 package com.mustache.bbs.domain;
 
-public class Response {
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class Response<T> {
+
+    // 어떤 에러가 났는지 반환해주는 문자열
+    private String resultCode;
+    // 성공을 반환할 때 result로 감싸서 리턴.
+    private T result;
+
+    private static Response<Void> error(String resultCode) {
+        return new Response(resultCode, null);
+    }
+
+    public static <T> Response<T> success(T result){
+        return new Response("SUCCESS", result);
+    }
+
 }
