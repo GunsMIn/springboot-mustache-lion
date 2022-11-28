bb358bf1cc3fd7d70f5f7008182465a204752649
KimGunWoo9595 <medneter@naver.com>
2022-11-28 13:48:04 +0900

    회원컨트롤러(회원가입)

--- src/main/java/com/mustache/bbs/controller/UserController.java
+++ src/main/java/com/mustache/bbs/controller/UserController.java
@@ -6,20 +6,23 @@ import com.mustache.bbs.domain.user.UserJoinRequest;
 import com.mustache.bbs.domain.user.UserJoinResponse;
 import com.mustache.bbs.service.UserService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/users")
-@RestController
+@RestController @Slf4j
 public class UserController {
 
     private final UserService userService;
 
     @PostMapping("/join")
     public Response<UserJoinResponse> join(@RequestBody UserJoinRequest userJoinRequest) {
+        log.info("userJoinRequest :{} ", userJoinRequest);
         UserDto userDto = userService.join(userJoinRequest);
-        return Response.success(new UserJoinResponse(userDto.getUserName(), userDto.getPassword()));
-    }
+        log.info("userDto :{} ", userDto);
+        return Response.success(new UserJoinResponse(userDto.getUserName(), userDto.getEmailAddress()));
+    } 
 }
