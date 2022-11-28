60e8ecea69859ddfd680bc62ce88666c7eeab443
KimGunWoo9595 <medneter@naver.com>
2022-11-28 15:07:39 +0900

    ExceptionManager

--- src/main/java/com/mustache/bbs/exceptionManager/ExceptionManager.java
+++ src/main/java/com/mustache/bbs/exceptionManager/ExceptionManager.java
@@ -7,12 +7,21 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
-public class ExceptionManeger {
+public class ExceptionManager {
 
-    @ExceptionHandler(RuntimeException.class)
+   /* @ExceptionHandler(RuntimeException.class)
     public ResponseEntity<?> runtimeExceptionHandler(RuntimeException e) {
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
                 .body(Response.error(e.getMessage()));
+    }*/
+
+    @ExceptionHandler(HospitalReviewException.class)
+    public ResponseEntity<?> hospitalReviewAppExceptionHandler(HospitalReviewException e) {
+        return ResponseEntity.status(e.getErrorCode().getStatus())
+                .body(Response.error(e.getErrorCode().getMessage()));
     }
-    
+
+
+
+
 }
