fbe5bd4970888d925cf59d42f6a6084dc0099e2b
KimGunWoo9595 <medneter@naver.com>
2022-11-28 15:07:24 +0900

    커스텀예외

--- src/main/java/com/mustache/bbs/exceptionManager/HospitalReviewException.java
+++ src/main/java/com/mustache/bbs/exceptionManager/HospitalReviewException.java
@@ -1,4 +1,19 @@
 package com.mustache.bbs.exceptionManager;
 
-public class HospitalReviewException {
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor @Getter
+public class HospitalReviewException extends RuntimeException{
+
+    ErrorCode errorCode;
+    private String message;
+
+    @Override
+    public String toString() {
+        if(message == null) {
+            return errorCode.getMessage();
+        }
+        return String.format("%s. %s", errorCode.getMessage(), message);
+    }
 }
