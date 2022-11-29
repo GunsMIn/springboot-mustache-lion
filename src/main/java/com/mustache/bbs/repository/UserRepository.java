a83fb87d879b6cd8f09c8acf1b1e52f9ef9f69c9
KimGunWoo9595 <medneter@naver.com>
2022-11-29 14:24:04 +0900

    repositroy 메소드 추가

--- src/main/java/com/mustache/bbs/repository/UserRepository.java
+++ src/main/java/com/mustache/bbs/repository/UserRepository.java
@@ -4,10 +4,13 @@ import com.mustache.bbs.domain.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User,Long> {
 
     List<User> findByUsername(String username);
 
     List<User> findByEmailAddress(String emailAddress);
+
+    Optional<User> findUserByUsername(String username);
 }
