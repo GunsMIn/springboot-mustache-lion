468672fbf7ae7d4fe39cc019b34b382b4e83e8f4
KimGunWoo9595 <medneter@naver.com>
2022-11-29 14:23:41 +0900

    errorCode

--- src/main/java/com/mustache/bbs/exceptionManager/ErrorCode.java
+++ src/main/java/com/mustache/bbs/exceptionManager/ErrorCode.java
@@ -9,8 +9,8 @@ public enum ErrorCode {
 
     DUPLICATED_USER_NAME(HttpStatus.CONFLICT, "User name is duplicated"),
     INTERNAR_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "Internal server error"),
-    USER_NOT_FOUNDED(HttpStatus.NOT_FOUND, "해당 유저를 찾을 수 없습니다"),
-    INVALID_PASSWORD(HttpStatus.BAD_REQUEST, "비밀번호가 맞지 않습니다");
+    NOT_FOUNDED(HttpStatus.NOT_FOUND, "Not Found"),
+    INVALID_PASSWORD(HttpStatus.BAD_REQUEST, "Not correct password");
 
     private HttpStatus status;
     private String message;
