90da8bb4210731090c7882f92d9ec55de328c3e7
KimGunWoo9595 <medneter@naver.com>
2022-12-03 14:10:12 +0900

    update errorMessage

--- src/main/java/com/mustache/bbs/exceptionManager/ErrorCode.java
+++ src/main/java/com/mustache/bbs/exceptionManager/ErrorCode.java
@@ -7,10 +7,10 @@ import org.springframework.http.HttpStatus;
 @AllArgsConstructor @Getter
 public enum ErrorCode {
 
-    DUPLICATED_USER_NAME(HttpStatus.CONFLICT, "User name is duplicated"),
-    INTERNAR_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "Internal server error"),
-    NOT_FOUNDED(HttpStatus.NOT_FOUND, "Not Found"),
-    INVALID_PASSWORD(HttpStatus.BAD_REQUEST, "Not correct password");
+    DUPLICATED_USER_NAME(HttpStatus.CONFLICT, "User name is duplicated errorCode"),
+    INTERNAR_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "Internal server error errorCode"),
+    NOT_FOUNDED(HttpStatus.NOT_FOUND, "Not Found errorCode"),
+    INVALID_PASSWORD(HttpStatus.BAD_REQUEST, "Not correct password errorCode");
 
     private HttpStatus status;
     private String message;
