7e7637dd1c3b878bbcc2350544cfc6d96aa917b3
KimGunWoo9595 <medneter@naver.com>
2022-12-05 14:30:41 +0900

    ReviewRequest

--- src/main/java/com/mustache/bbs/domain/dto/review/ReviewCreateRequest.java
+++ src/main/java/com/mustache/bbs/domain/dto/review/ReviewCreateRequest.java
@@ -1,4 +1,14 @@
 package com.mustache.bbs.domain.dto.review;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Data @Getter @NoArgsConstructor
 public class ReviewCreateRequest {
+    private Long hospitalId;
+    private String title;
+    private String content;
+    private String userName;
 }
