4c823fbfd6ef6524c3d5a1008811a358dade50d0
KimGunWoo9595 <medneter@naver.com>
2022-12-05 11:29:45 +0900

    Review RestController

--- src/main/java/com/mustache/bbs/controllerRest/ReviewController.java
+++ src/main/java/com/mustache/bbs/controllerRest/ReviewController.java
@@ -1,4 +1,18 @@
 package com.mustache.bbs.controllerRest;
 
+import com.mustache.bbs.domain.dto.review.ReviewCreateRequest;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/reviews")
 public class ReviewController {
+
+    @PostMapping
+    public String write(@RequestBody ReviewCreateRequest reviewCreateRequest) {
+
+        return "리뷰 등록에 성공 했습니다";
+    }
 }
