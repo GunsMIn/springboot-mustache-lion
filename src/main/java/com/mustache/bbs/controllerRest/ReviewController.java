902a0612e84e347ca0542269b5c7b90bb10eebb9
KimGunWoo9595 <medneter@naver.com>
2022-12-05 16:08:32 +0900

    인증된 토큰을 발급받은 회원만 리뷰작성 API

--- src/main/java/com/mustache/bbs/controllerRest/ReviewController.java
+++ src/main/java/com/mustache/bbs/controllerRest/ReviewController.java
@@ -1,18 +1,22 @@
 package com.mustache.bbs.controllerRest;
 
 import com.mustache.bbs.domain.dto.review.ReviewCreateRequest;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@RestController @Slf4j
 @RequestMapping("/api/v1/reviews")
 public class ReviewController {
 
+    //SpringSecurity에서 인증된 토큰을 얻은 User만 review를 쓸 수 있는 API
     @PostMapping
-    public String write(@RequestBody ReviewCreateRequest reviewCreateRequest) {
+    public String write(@RequestBody ReviewCreateRequest dto, Authentication authentication) {
+        log.info("Controller user:{}", authentication.getName());
+        return "리뷰 등록에 성공 했습니다.";
 
-        return "리뷰 등록에 성공 했습니다";
     }
 }
