6d31cbb006265b1c112916b9998bb87466465c36
KimGunWoo9595 <medneter@naver.com>
2022-12-05 16:15:19 +0900

    JwtTokenFilter(토큰 유효성검사,만료 토큰 검사)

---
+++ src/main/java/com/mustache/bbs/utils/JwtTokenFilter.java
@@ -0,0 +1,82 @@
+package com.mustache.bbs.utils;
+
+import com.mustache.bbs.domain.entity.User;
+import com.mustache.bbs.service.UserService;
+
+import com.mustache.bbs.utils.JwtTokenUtil;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpHeaders;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+import java.util.Date;
+import java.util.List;
+ /*{
+    "hospitalId":"2",
+   "title":"이 병원 좋아요",
+   "content":"병원이 매우 쾌적하고 의사선생님이 잘해주세요",
+   "userName":"김건우"
+}*/
+
+@RequiredArgsConstructor
+@Slf4j
+public class JwtTokenFilter extends OncePerRequestFilter {
+
+    private final UserService userService;
+    private final String secretKey;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+
+        final String authorizationHeader = request.getHeader(HttpHeaders.AUTHORIZATION);
+        log.info("authorizationHeader:{}", authorizationHeader);
+
+        if (authorizationHeader == null || !authorizationHeader.startsWith("Bearer ")) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+        // token분리
+        String token;
+
+        try {
+            token = authorizationHeader.split(" ")[1];
+        } catch (Exception e) {
+            log.error("token 추출에 실패 했습니다.");
+            filterChain.doFilter(request, response);
+            return;
+        }
+        // Token이 만료 되었는지 Check
+        if(JwtTokenUtil.isExpired(token, secretKey)){
+            filterChain.doFilter(request, response);
+            return;
+        };
+
+        // token에서 userName 꺼내기
+        String userName = JwtTokenUtil.getUserName(token, secretKey);
+        log.info("사용자 이름 : {}",userName);
+
+        User user = userService.getUserByUserName(userName);
+        log.info("userRole : {} ",user.getRole());
+
+        //문 열어주기, Role 바인딩
+        UsernamePasswordAuthenticationToken authenticationToken =
+                new UsernamePasswordAuthenticationToken(user.getUsername(), null, List.of(new SimpleGrantedAuthority(user.getRole().name())));
+        authenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+        SecurityContextHolder.getContext().setAuthentication(authenticationToken); // 권한 부여
+        filterChain.doFilter(request, response);
+
+    }
+}
