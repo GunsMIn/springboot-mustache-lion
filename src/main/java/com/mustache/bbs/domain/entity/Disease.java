775f6747c77bd60402a82eb4c848c3daf8ba462e
KimGunWoo9595 <medneter@naver.com>
2022-12-07 21:25:53 +0900

    add disease

--- src/main/java/com/mustache/bbs/domain/entity/Disease.java
+++ src/main/java/com/mustache/bbs/domain/entity/Disease.java
@@ -6,11 +6,13 @@ import lombok.Getter;
 
 @AllArgsConstructor @Getter
 public enum Disease {
-
     A(1, "감기"),
     B(2, "위염"),
     C(3, "장염"),
-    D(4, "코로나");
+    D(4, "코로나"),
+    E(5, "근육통"),
+    F(6, "코막힘"),
+    G(7, "원인불명");
 
     private Integer code;
     private String name;
