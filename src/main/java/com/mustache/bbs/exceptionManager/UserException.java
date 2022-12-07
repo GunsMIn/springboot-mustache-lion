3028f5d0402b56a6fb5cc9b4ab48fa0753905beb
KimGunWoo9595 <medneter@naver.com>
2022-12-07 17:45:00 +0900

    userException

---
+++ src/main/java/com/mustache/bbs/exceptionManager/UserException.java
@@ -0,0 +1,21 @@
+
+package com.mustache.bbs.exceptionManager;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor @Getter
+public class UserException extends RuntimeException{
+
+    ErrorCode errorCode;
+    private String message; //String.format("UserName:%s", userJoinRequest.getUserName())
+
+    @Override
+    public String toString() {
+        if(message == null) {
+            return errorCode.getMessage();
+        }
+        return String.format("%s. %s", errorCode.getMessage(), message);
+    }
+}
+
