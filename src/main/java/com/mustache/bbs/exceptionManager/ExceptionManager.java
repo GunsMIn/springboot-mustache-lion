f40e24caa3fd40f1521c86ba6c3c254e5b5586f7
KimGunWoo9595 <medneter@naver.com>
2022-12-07 17:47:47 +0900

    exception 수정

--- src/main/java/com/mustache/bbs/exceptionManager/ExceptionManager.java
+++ src/main/java/com/mustache/bbs/exceptionManager/ExceptionManager.java
@@ -20,13 +20,7 @@ public class ExceptionManager {
         return ResponseEntity.status(HttpStatus.BAD_REQUEST) // 5xx에러를 400에러로 바꿔줌
                 .body(Response.error(e.getMessage()));
     }
-
-    //도커 배포전 수정
-    @ExceptionHandler(UserException.class)
-    public  ResponseEntity<?> userAppExceptionHandler(UserException e) {
-        return ResponseEntity.status(e.getErrorCode().getStatus())
-                .body(Response.error(e.getErrorCode().getMessage()));
-    }
+    
 
 
 
