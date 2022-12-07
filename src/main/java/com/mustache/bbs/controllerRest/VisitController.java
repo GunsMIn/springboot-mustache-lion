e12be8234127e7ff9cf2fdb95baf9955aa12464f
KimGunWoo9595 <medneter@naver.com>
2022-12-07 13:46:46 +0900

    VisitController 방문 생성 POST/api/v1/create/{userName}

--- src/main/java/com/mustache/bbs/controllerRest/VisitController.java
+++ src/main/java/com/mustache/bbs/controllerRest/VisitController.java
@@ -7,6 +7,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,13 +16,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 @Slf4j
 public class VisitController {
-
+    //http://localhost:8909/api/v1/create/김건우
+    // "hospitalId":"2",
+    //  "disease" :"B",
+    //  "count":"4"
     private final VisitService visitService;
 
-    @PostMapping("/api/v1/create")
-    public ResponseEntity<VisitCreateResponse> create(@RequestBody VisitCreateRequest visitCreateRequest, Authentication authentication) {
+    @PostMapping("/api/v1/create/{userName}")
+    public ResponseEntity<VisitCreateResponse> create(@RequestBody VisitCreateRequest visitCreateRequest, @PathVariable  String userName, Authentication authentication) {
         log.info("Controller user:{},authentication :{}", authentication.getName(),authentication.isAuthenticated());
-        VisitCreateResponse visitCreateResponse = visitService.create(visitCreateRequest);
+        VisitCreateResponse visitCreateResponse = visitService.create(visitCreateRequest,userName);
         return ResponseEntity.ok().body(visitCreateResponse);
     }
 
