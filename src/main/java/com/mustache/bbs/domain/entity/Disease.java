8161850464f1bee8def93edeb740e53ca7eb1da3
KimGunWoo9595 <medneter@naver.com>
2022-12-07 13:18:52 +0900

    Disease 수정

--- src/main/java/com/mustache/bbs/domain/entity/Disease.java
+++ src/main/java/com/mustache/bbs/domain/entity/Disease.java
@@ -1,6 +1,17 @@
 package com.mustache.bbs.domain.entity;
 
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor @Getter
 public enum Disease {
-    code,name
+
+    A(1, "감기"),
+    B(2, "위염"),
+    C(3, "장염"),
+    D(4, "코로나");
+
+    private Integer code;
+    private String name;
 }
