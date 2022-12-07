422fa69ffb34f102af51a72db788bf52e31b4b4b
KimGunWoo9595 <medneter@naver.com>
2022-12-07 14:09:52 +0900

    Visit 단건 조회용 DTO

---
+++ src/main/java/com/mustache/bbs/domain/dto/visit/VisitSelectResponse.java
@@ -0,0 +1,31 @@
+package com.mustache.bbs.domain.dto.visit;
+
+
+import com.mustache.bbs.domain.entity.Disease;
+import com.mustache.bbs.domain.entity.Hospital;
+import com.mustache.bbs.domain.entity.User;
+import com.mustache.bbs.domain.entity.Visit;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data  @NoArgsConstructor
+public class VisitSelectResponse {
+
+    private Long id;
+
+    private String userName;
+
+    private Disease disease;
+
+    private Integer count;
+
+    public VisitSelectResponse(Visit visit) {
+        this.id = visit.getId();
+        this.userName = visit.getUser().getUsername();
+        this.disease = visit.getDisease();
+        this.count = visit.getCount();
+    }
+}
